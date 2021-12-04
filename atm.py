9f667a56ce754dbda866f3afe2b2197e6025f48a
Kaguinewme <95537815+Kaguinewme@users.noreply.github.com>
2021-12-04 11:48:48 -0500

    Add files via upload

---
+++ atm.py
@@ -0,0 +1,61 @@
+# print("welcome to atm machine")
+# language=input("enter the language:")
+# if language=="english":
+#     print("welcome to punjab national bank of india\swipe your card:")
+#     balance=60000
+#     print("enter your transaction:\n1.balance enquiry:\n2.withdraw:\n3deposit:\n4.transfer money:\n5.exit")
+#     transaction=int(input("enter the transaction:"))
+#     if transaction==1 or transaction==2 or transaction==3 or transaction==4 :
+#         atm_pin=int(input("enter the pin:"))
+#         if atm_pin==7890:
+#             if transaction==1:
+#                 print("collect your card\ .thank you")
+#                 print("balance is:",balance)
+               
+#             elif transaction==2:
+#                 withdraw_amount=int(input("enter the withdraw amount"))
+#                 if withdraw_amount<=balance:
+#                    total=balance-withdraw_amount
+#                    print(total)
+#                    print("collect your cash\.thank you")
+#                 else:
+#                     print("you have no balance")
+#             elif transaction==3:
+#                 deposit=int(input("enter the deposit:"))
+#                 if deposit>0:
+#                     total=balance+deposit
+#                     print(total)
+#                     print("your deposit is successful\.thank you")
+#                 else:
+#                     print("no deposit")
+#             elif transaction==4:
+#                 transfer_money=int(input("enter the transfer_money:"))
+#                 if transfer_money>0:
+#                      total=balance-transfer_money
+#                      print(total)
+#                      print("your money has been transfer\.thank you")
+#                 else:
+#                      print("transfer failed")  
+#             else:
+#                print("wrong pin")
+#     elif transaction==5:
+#         exit=input("do you want exit:")
+#         if exit=="yes":
+#             print("thank you for visiting")
+#         else:
+#             print("choose your transaction")
+# else:
+#     print("language does n0t exist")
+                
+        
+                
+                
+                
+                
+                
+                
+            
+    
+
+    
+    
---
+++ interview.py
@@ -0,0 +1,97 @@
+# if"true":
+#     print("A")
+# if False:
+#     print("B")
+# elif True:
+#     print("C")
+# if True:
+
+#     print("D")
+# if True:
+#     print("E")
+# elif True:
+#     print("F")
+# else:
+#     pass
+
+# if True:
+#     print("a")
+# elif True:
+#     print("b")
+# else:
+#     print("c")
+
+# if True:
+#     print("a")
+# elif True:
+#     print("b")
+
+# if True:
+#     print("a")
+# if "True":
+#     print("b")
+# if False:
+#     print("c")
+# elif "True":
+#     print("d")
+# else:
+#     print("e")
+
+# if True:
+#     print("a")
+# if "False":
+#     print("b")
+# if "False":
+#     print("c")
+# elif False:
+#     print("d")
+# else:
+#     print("e")
+
+# if "false":
+#     print("a") 
+# if False:
+#     print("b")
+# elif "True":
+#     print("c")
+# elif "False":
+#     print("d")
+# if True:
+#     print("e")
+# else:
+#      print("f") 
+    
+
+# if True:
+#     print("a")
+# if True:
+#     print("b")
+# if True:
+#     print("c")
+# elif True:
+#     print("d")
+# else:
+#     print(e)
+
+# if 0:
+#     print("s")
+# else:
+#     print("a")
+  
+# if -1:
+#     print("zero")
+# else:
+#     print("one")
+
+# if "False":
+#     print("a")
+# if False:
+#     print("b")
+# elif True:
+#     print("c")
+# else:
+#     print("d")
+
+    
+    
+    
