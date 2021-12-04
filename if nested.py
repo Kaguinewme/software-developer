97b514403767f710d2ade1db510728df1834e3c4
Kaguinewme <95537815+Kaguinewme@users.noreply.github.com>
2021-12-04 10:30:20 -0500

    Add files via upload

---
+++ if nested.py
@@ -0,0 +1,107 @@
+# covid_test=input("did you test?\n1.yes\n2.n0\n")
+# if covid_test=="yes":
+#     report=input("report:\n1.positive \n2.negative\n")
+#     if report=="positive":
+#         print("stay with us")
+#     elif report=="negative":
+#         print("dont stay with us")
+#     else:
+#         pass
+
+# age=int(input("enter the age"))
+# sex=input("enter the sex:m or f")
+# days=int(input("enter the number of days"))
+# if age>=18 and age<30:
+#     if sex=="m":
+#         wage=days*700
+#         print(wage)
+#     else:
+#         wage=days*750
+#         print(wage)
+# elif age>=30 and age<=40:
+#     if sex=="f":
+#         wage=days*850
+#         print(wage)
+# else:
+#     wage=days*800
+#     print(wage)
+
+    
+# year=int(input("enter your year"))
+# if (year%4)==0:
+#     if year%100==0:
+#         if year%400==0:
+#             print("leap year")
+#         else:
+#             print("not a leap year")
+#     else:
+#         print("leap year")
+# else:
+#     print("not leap year")
+
+# a=int(input("enter the number"))
+# b=int(input("enter the number"))
+# c=int(input("enter the number"))
+# if a>b :
+#     if a>c:
+#         print(a," is maximum")
+#     elif b>c:
+#         print(b, "is maximum")
+#     else:
+#         print(c,"is maximum")
+# elif b>c:
+#     if b>a:
+#         print(b," is maximum")
+#     elif c>b:
+#         print(c,"is maximum")
+#     else:
+#         print(a,'is maximum')
+# elif c>a:
+#     if c>b:
+#        print(c,"is maximum")
+#     elif a>c:
+#         print(a,'is maximum')
+#     else:
+#         print(b,"is maximum")   
+# else:
+#     print("invalid condition")
+
+# day=input("enter the day")
+# meal_time=input("enter the meal time")
+# if day=="monday":
+#     if meal_time=="breakfast":
+#         print("poori sabji")
+#     elif meal_time=="lunch":
+#         print("sambhar rice")
+#     else:
+#         print("chicken rice")
+# elif day=="tuesday":
+#     if meal_time=="breakfast":
+#         print("poha")
+#     elif meal_time=="lunch":
+#         print("rajma rice")
+#     else:
+#         print("roti sabji")
+# else:
+#     print("anything ")
+
+
+# print("welcome to the navgurukul organisation")
+# algebra_test=input("Enter the algebra-test result:yes or no=")
+# if algebra_test=="yes":
+#     print("You are selected for English test")
+#     english_test=input("Enter the english_test result:yes or no =")
+#     if english_test=="yes":
+#         print("Congratulation !! you are selected for  navgurukul one year  course.")
+#     else:
+#         print("Try again")
+# else: 
+#     print("Better luck next time.")
+
+        
+   
+    
+        
+        
+
+
